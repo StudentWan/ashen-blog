4140c917796d763432b27a8b33a152d8cf02b162
StudentWan <benyuwan@gmail.com>
2017-10-12 20:17:33 +0800

    modify: webpack resolver && add: server

--- client/build/webpack.base.conf.js
+++ client/build/webpack.base.conf.js
@@ -22,8 +22,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      'vue$': 'vue/dist/vue.esm.js'
     }
   },
   module: {
