7ee55a2744bc4a444e2cde898f02ce12b1627474
StudentWan <benyuwan@gmail.com>
2017-10-16 13:54:47 +0800

    add: iview && sass loader

--- admin/build/webpack.base.conf.js
+++ admin/build/webpack.base.conf.js
@@ -22,7 +22,8 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
-      'vue$': 'vue/dist/vue.esm.js'
+      'vue$': 'vue/dist/vue.esm.js',
+      'iview$': 'iview'
     }
   },
   module: {
@@ -40,6 +41,10 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: vueLoaderConfig
+      },
+      {
+        test: '/iview.src.*?js$/',
+        loader: 'babel-loader'
       },
       {
         test: /\.js$/,
--- admin/src/main.js
+++ admin/src/main.js
@@ -5,6 +5,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import 'iview/dist/styles/iview.css'
 import App from './App.vue'
 import router from './router/index'
 
