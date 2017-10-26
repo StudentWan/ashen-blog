2e0319b121a1f8038558dfb02b156efae358c43b
StudentWan <benyuwan@gmail.com>
2017-10-26 15:50:42 +0800

    add: highlightjs theme && enhance markdown render

--- client/src/test.js
+++ client/src/test.js
@@ -46,12 +46,20 @@ export default {
 
 
 
-\`\`\`javascript1
-app.use(function(req, res) {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
-});
+\`\`\`javascript
+import Vue from 'vue'
+import Router from 'vue-router'
+import routes from './routes'
+import Blog from './Blog'
+import {markdown} from './filters/index'
+import './stylus/index.styl'
+
+Vue.use(Router)
+Vue.filter('markdown', markdown);
+
+const router = new Router()
+routes(router)
+router.start(Blog, 'blog')
 \`\`\`
     `
 }
